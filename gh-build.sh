0c3da0e51863b154c7b5974b1eba6bb3bb6c71e0
Uri Herrera <hello@uriherrera.com>
2022-11-04 22:22:56 -0600

    update pkg

---
+++ gh-build.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+apt -qq update
+apt -qq -yy install equivs curl git
+
+git config --global --add safe.directory /home/travis/build/Nitrux/nx-desktop
+
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
+git_commit=$(git rev-parse --short HEAD)
+
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: nx-desktop-wayland-legacy" \
+	"Version: 0.0.5-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Conflicts: nx-desktop-wayland" \
+	"Architecture: amd64" \
+	"Description: NX Desktop metapackage for Nitrux."
+
+equivs-build configuration
